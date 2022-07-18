992fd15b912afce9c457297a09fce2a566a9dc6f
uwumouse <pythonisajoke@gmail.com>
2022-07-18 20:47:39 +0300

    Implement custom blocks

--- src/engine.rs
+++ src/engine.rs
@@ -1,7 +1,7 @@
 use crate::config::{Config, FileConfig, ThemeVars};
 use colored::Colorize;
 use regex::{Regex, RegexBuilder};
-use std::{fs, process::exit};
+use std::{collections::HashSet, fs, process::exit};
 
 pub fn update_configs(theme_name: String, config: Config) {
     let theme = match config.themes.get(&theme_name) {
@@ -16,11 +16,8 @@ pub fn update_configs(theme_name: String, config: Config) {
         }
     };
 
-    for (name, conf) in config.files {
-        // TODO:
-        //if let Some(custom) = conf.custom {
-        //}
-        let contents = match fs::read_to_string(&conf.path) {
+    for (_, conf) in config.files {
+        let mut contents = match fs::read_to_string(&conf.path) {
             Ok(f) => f,
             Err(e) => {
                 log::error!("Error reading `{}`:\n{e}", conf.path);
@@ -34,11 +31,54 @@ pub fn update_configs(theme_name: String, config: Config) {
             exit(1);
         }
 
-        let new_block = vars_to_themer_block(&theme, &conf);
-        let contents = themer_block_re.replacen(&contents, 1, new_block);
+        let mut new_block;
+
+        if let Some(mut custom) = conf.custom.clone() {
+            for var in get_custom_block_vars(&custom) {
+                match var.as_str() {
+                    "<colors>" => custom = custom.replace("<colors>", &format_vars(&theme, &conf)),
+                    "<name>" => custom = custom.replace("<name>", &theme_name),
+                    var => {
+                        let plain_var = var.replace("<", "").replace(">", "");
+
+                        if let Some(v) = theme.get(&plain_var) {
+                            custom = custom.replace(var, v);
+                        } else {
+                            log::warn!(
+                                "Custom block for file `{}`: variable {var} cannot be found.",
+                                conf.path
+                            );
+                        }
+                    }
+                };
+            }
+
+            new_block = custom;
+        } else {
+            new_block = format_vars(&theme, &conf);
+        }
+
+        new_block = wrap_with_themer_block(new_block, &&conf.comment);
+
+        contents = themer_block_re
+            .replacen(&contents, 1, new_block)
+            .to_string();
 
         fs::write(&conf.path, contents.as_bytes()).unwrap();
     }
+}
+
+/// Finds unique variables in contents block
+fn get_custom_block_vars(contents: &String) -> Vec<String> {
+    lazy_static::lazy_static! {
+        static ref RE: Regex = Regex::new("<.*>").unwrap();
+    }
+
+    RE.find_iter(contents)
+        .map(|x| x.as_str().to_string())
+        .collect::<HashSet<String>>()
+        .into_iter()
+        .collect()
 }
 
 pub fn get_block_re(comment: &String) -> Regex {
@@ -48,8 +88,14 @@ pub fn get_block_re(comment: &String) -> Regex {
         .unwrap()
 }
 
-fn vars_to_themer_block(vars: &ThemeVars, config: &FileConfig) -> String {
-    let mut block = String::from("\n");
+fn wrap_with_themer_block(contents: String, comment: &String) -> String {
+    // Block is surrounded with newlines so no need to devide comment lines and the actual block
+    // in this format! call
+    format!("{0} THEMER\n{contents}\n{0} THEMER_END", comment)
+}
+
+fn format_vars(vars: &ThemeVars, config: &FileConfig) -> String {
+    let mut block = String::new();
 
     for (key, val) in vars {
         block.push_str(
@@ -62,14 +108,12 @@ fn vars_to_themer_block(vars: &ThemeVars, config: &FileConfig) -> String {
         block.push('\n');
     }
 
-    // Block is surrounded with newlines so no need to devide comment lines and the actual block
-    // in this format! call
-    format!("{0} THEMER{block}{0} THEMER_END", config.comment)
+    block.trim_end().to_owned()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::vars_to_themer_block;
+    use super::format_vars;
     use crate::config::FileConfig;
     use std::collections::HashMap;
 
@@ -86,7 +130,7 @@ mod tests {
             custom: None,
         };
 
-        let res = vars_to_themer_block(&vars, &conf);
+        let res = format_vars(&vars, &conf);
         assert_eq!(
             res,
             r#"# THEMER
