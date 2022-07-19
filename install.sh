de7b55701bc90e25fbb52f9a78825359823083dc
uwumouse <pythonisajoke@gmail.com>
2022-07-19 11:35:50 +0300

    Fix install script

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 echo "This script will install Themer into /usr/bin and create a directory inside of ~/.config"
-read -p "Press [ENTER] to continue installation"
+read -p "Press [ENTER] to continue installation: "
 
-sudo curl https://gitlab.com/themer-cli/themer/uploads/542bfa01e8e0cf1c28959b170e6f16f3/themer --output /usr/bin/themer
-sudo chmod +x /usr/bin/themer
+sudo curl https://gitlab.com/themer-cli/themer/uploads/542bfa01e8e0cf1c28959b170e6f16f3/themer --output /usr/bin/themer && \
+sudo chmod +x /usr/bin/themer && \
 
-mkdir -p ~/.config/themer
-wget https://gitlab.com/themer-cli/themer/-/raw/main/assets/config.yml -O ~/.config/themer/config.yml
+mkdir -p ~/.config/themer && \
+wget https://gitlab.com/themer-cli/themer/-/raw/main/assets/config.yml -O ~/.config/themer/config.yml && \
 
-echo "Themer successfully installed."
+echo "Themer successfully installed." && \
 echo "Check out default configuration file inside of ~/.config/themer/themer.yml"
