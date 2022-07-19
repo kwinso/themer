965cec97e59e9530f4a8587347027fb5812d16b3
uwumouse <pythonisajoke@gmail.com>
2022-07-19 11:47:18 +0300

    Update path for the latest release

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "This script will install Themer into /usr/bin and create a directory inside of ~/.config"
 read -p "Press [ENTER] to continue installation: "
 
-sudo curl https://gitlab.com/themer-cli/themer/uploads/542bfa01e8e0cf1c28959b170e6f16f3/themer --output /usr/bin/themer && \
+sudo curl https://gitlab.com/themer-cli/themer/uploads/cc3f4eb318802ae1ed694cde1e78ac24/themer --output /usr/bin/themer && \
 sudo chmod +x /usr/bin/themer && \
 
 mkdir -p ~/.config/themer && \
